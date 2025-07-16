4e78fd3fc7e58f199f9c7b02e2601098189f9230
alessandralessamc <alessandra.lessa@gmail.com>
2025-07-16 20:33:20 +0000

    Commit inicial - Alessandra

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Commit inicial - Alessandra Lessa McNamee
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
